b3bffd5b6a11e142e4596fd2ecc494b25eca2bec
tsu-nera <fox10225fox@gmail.com>
2022-02-08 08:52:20 +0900

    wip

--- src/meigen_bot/core.clj
+++ src/meigen_bot/core.clj
@@ -1,8 +1,12 @@
 (ns meigen-bot.core
   (:require [clj-http.client :as client]))
 
-(client/get "https:futurismo.biz")
 
-(defn get-status
-  [status-id]
+;; url = 'https://api.twitter.com/2/timeline/conversation/{}.json?include_reply_count=1&send_error_codes=true&count={}{}'.format(status_id, str(count), cursor)
+
+(def get-statuses
+  [let [url "https://api.twitter.com/2/timeline/conversation/xxx.json"]
+   (client/get url)])
+
+(defn get-status [status-id]
   (println "Get Status: id="status-id ))
