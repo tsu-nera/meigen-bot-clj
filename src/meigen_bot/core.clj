794ffb086d3288665f79e71c875614e06d4287db
tsu-nera <fox10225fox@gmail.com>
2022-02-09 19:47:15 +0900

    :sparkles:private apiによるツイート取得

--- src/meigen_bot/core.clj
+++ src/meigen_bot/core.clj
@@ -1,10 +1,10 @@
 (ns meigen-bot.core
   (:require ;; [meigen-bot.twitter.guest-client :as guest]
-            [meigen-bot.twitter.privat-client :as private]))
+   [meigen-bot.twitter.private-client :as private]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; (def status (guest/get-status "1477034578875277316"))
-(def status (private/get-status "1477034578875277316"))
+;; (def status (private/get-status "1477034578875277316"))
 
 ;; status
--- src/meigen_bot/twitter/private_client.clj
+++ src/meigen_bot/twitter/private_client.clj
@@ -6,40 +6,36 @@
 (def auth-token (env :auth-token))
 (def ct0 (env :ct0))
 
+(def guest-bearer-token
+  "AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA")
 (def user-agent "Mozilla/5.0 (X11; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0")
 
-;; (def guest-token
-;;   (delay
-;;     (let [response (client/post
-;;                     "https://api.twitter.com/1.1/guest/activate.json"
-;;                     {:cookie-policy :none
-;;                      :headers       {:authorization (str "Bearer " guest-bearer-token)
-;;                                      :user-agent    user-agent}})]
-;;       (-> response
-;;           :body
-;;           (json/parse-string true)
-;;           :guest_token
-;;           (or (throw (ex-info "Can't get guest token" {:response response})))))))
+(def cookie (str "auth_token=" auth-token "; ct0=" ct0))
 
-;; (def guest-headers {:headers
-;;                     {:authorization (str "Bearer " guest-bearer-token)
-;;                      :user-agent    user-agent
-;;                      :x-guest-token @guest-token}})
+(def private-headers {:headers
+                      {:authorization (str "Bearer " guest-bearer-token)
+                       :user-agent    user-agent
+                       :x-csrf-token  ct0
+                       :cookie        cookie}})
 
-;; (defn get-statuses-url [status-id]
-;;   (str "https://api.twitter.com/2/timeline/conversation/"
-;;        status-id
-;;        ".json?include_reply_count=1&send_error_codes=true&count=20"))
+(defn get-statuses-url [status-id]
+  (str "https://api.twitter.com/2/timeline/conversation/"
+       status-id
+       ".json?include_reply_count=1&send_error_codes=true&count=20"))
 
-;; (defn get-statuses [status-id]
-;;   (let [url (get-statuses-url status-id)]
-;;     (client/get url guest-headers)))
+(defn get-statuses [status-id]
+  (let [url (get-statuses-url status-id)]
+    (client/get url private-headers)))
 
-;; (defn get-status [status-id]
-;;   (let [response (get-statuses status-id)]
-;;     (-> response
-;;         :body
-;;         (json/parse-string true)
-;;         :globalObjects
-;;         :tweetsq
-;;         (get (keyword status-id)))))
+(defn get-status [status-id]
+  (let [response (get-statuses status-id)]
+    (-> response
+        :body
+        (json/parse-string true)
+        :globalObjects
+        :tweets
+        (get (keyword status-id)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (def status (get-status "1477034578875277316"))
+;; status
